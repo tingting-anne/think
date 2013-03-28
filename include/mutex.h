6c908c1bddca9c6abed4970d6cbd81d4d1149fd2
yjhjstz <yjhjstz@163.com>
2013-03-28 20:16:48 +0800

    simplify mutex

--- include/mutex.h
+++ include/mutex.h
@@ -18,7 +18,7 @@ enum mutex_type
 class CMutex
 {
 public:
-	CMutex(int nType = MUTEX_NORMAL) : m_Type(nType)
+	CMutex(int nType = MUTEX_NORMAL)
 	{
 		pthread_mutexattr_t  attr;
 		if (pthread_mutexattr_init(&attr)) 
@@ -41,26 +41,6 @@ public:
 		pthread_mutex_destroy(&m_Mutex);
 	}
 	
-	bool  Locked()
-	{
-		int ret = pthread_mutex_trylock(&m_Mutex);
-
-		if (ret == EBUSY) 
-		{
-			return true;
-		}
-		else if (ret) 
-		{
-			//printf("try lock failed!\n");
-			return false;	
-		} 
-		else 
-		{
-			pthread_mutex_unlock(&m_Mutex);
-		}
-
-		return false;
-	}
 	
 	bool Enter()
 	{
@@ -69,24 +49,16 @@ public:
 	
 	bool Leave()
 	{
-		//assert(m_Mutex);
 		return pthread_mutex_unlock(&m_Mutex) == 0 ? true:false;
 	}
-	int getMutexType() const
-	{
-		return m_Type;
-	}
+	
 	pthread_mutex_t*  getMutex() 
 	{
 		return &m_Mutex;
 	}
 private:
 	pthread_mutex_t m_Mutex;
-	int				m_Type;
-	// disable assign and copy
-private:
-	//CMutex( const CMutex & );
-//	CMutex &operator=( const CMutex & );
+	
 };
 } //end of namespace
 #endif //__MUTEX_H__
